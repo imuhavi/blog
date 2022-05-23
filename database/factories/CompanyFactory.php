a415d1f256064befc359ee728220a100426020df
Ian Muhavi <ianmuhavi@gmail.com>
2022-05-23 23:22:47 +0300

    Added companies to the table

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name'=>$this->faker->company(),
+            'website'=>$this->faker->lastName(),
+            'email'=>$this->faker->email(),
+            'address'=>$this->faker->address()
+        ];
+    }
+}
